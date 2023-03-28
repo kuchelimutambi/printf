40da164f785a705cb37fccb912367cb298c6f452
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-28 08:51:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,37 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /* va_list */
-#include <stdlib.h> /* malloc, free */
-#include <unistd.h> /* write */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-/* helper functions */
-char* (*get_func(char i))(va_list);
-char *create_buffer(void);
-void write_buffer(char *buffer, int len, va_list list);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-/* printf functions */
+int _putchar(char c);
+int print_number(int n);
 int _printf(const char *format, ...);
-char *print_s(va_list list);
-char *print_c(va_list list);
-char *print_d(va_list list);
-char *itob(va_list list);
-char *rot13(va_list list);
-char *rev_string(va_list list);
-char *itoOctal(va_list list);
-
-/**
- * struct types - struct
- * @id: identifier of type to print (e.g. c means char)
- * @func: ptr to functions that print according to identifier (e.g. print_c)
- */
-
-typedef struct types
-{
-	char id;
-	char* (*func)(va_list);
-} print;
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *);
+int print_hl(uintptr_t n);
 
 #endif
