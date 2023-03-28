d5d162a201a3d81559ac5adecd4b872d007f4455
kucheli mutambi <kuchelideogratius@gmail.com>
2023-03-28 09:10:27 +0300

    Update rot13.c

--- rot13.c
+++ rot13.c
@@ -1,39 +1,31 @@
 #include "main.h"
+
 /**
- * rot13 - encrypts string with rot13
- * @list: string to change
- * Return: modified string
+ * *rot13 - encoding to ROT13
+ * @s: string to work
+ * Return: the result.
  */
-char *rot13(va_list list)
+int rot13(char *s)
 {
-	int i = 0;
-	char *str;
-	char *nstr;
+	int a, b, count = 0;
+	char first[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char second[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	/* store va_arg to only access argument once */
-	str = va_arg(list, char *);
-
-	/* malloc new variable to assign over new rot13 characters */
-	nstr = malloc(sizeof(char) * (_strlen(str) + 1));
-	if (nstr == NULL)
-		return (NULL);
-
-	/* iterate str and assign rot13 char to nstr */
-	while (str[i] != '\0')
+	a = 0;
+	while (s[a] != 0)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+		if ((s[a] >= 'A' && s[a] <= 'Z') || (s[a] >= 'a' && s[a] <= 'z'))
 		{
-			nstr[i] = str[i] + 13;
-		}
-		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
-		{
-			nstr[i] = str[i] - 13;
-		}
-		else
-			(nstr[i] = str[i]);
-		i++;
+			for (b = 0; b < 52; b++)
+			{
+				if (s[a] == first[b])
+				{
+					count += _putchar(second[b]);
+				}
+			}
+		} else
+			count += _putchar(s[a]);
+		a++;
 	}
-	nstr[i] = '\0';
-
-	return (nstr);
+	return (count);
 }
